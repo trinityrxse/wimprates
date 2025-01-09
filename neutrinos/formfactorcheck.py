spectra need scaling, need to fix EW_STEPPING & RRPA

neutrinos/formfactorcheck.py
@@ -27,19 +27,22 @@ def helm(q, R):
     #print(qR, 'qR')
     if qR < 1e-12:
         return 1.0
-    #print((3 * sph_bessel_j1(qR)) * ((np.exp(-0.5 * (q * sparam)**2))/qR))
+    
+    #print((3 * sph_bessel_j1(qR)) * ((np.exp(-0.5 * (q * sparam)**2))/qR), 'helm')
     return (3 * sph_bessel_j1(qR)) * ((np.exp(-0.5 * (q * sparam)**2))/qR)
 
 # Helm radius in [fm]
 def helm_radius_fm(A):
     cparam = 1.23 * (A**(1.0 / 3.0)) - 0.6  # [fm]
     aparam = 0.52  # [fm]
+
     sparam = 0.9   # [fm]
     term = (7.0 / 3.0) * (PI**2) * (aparam**2) - 5 * (sparam**2)
     return np.sqrt(cparam**2 + term)
 
 # Helm form factor for a nucleus
 def helm_form_factor(Er_keV, A, mass_GeV):
+    #print('made here')
     R_eff = helm_radius_fm(A)  # [fm]
     q = recoil_to_q_ifm(Er_keV, mass_GeV)  # [fm^-1]
     return helm(q, R_eff)

neutrinos/neutrino_flux.py
@@ -55,7 +55,7 @@ class VNeutrinoCrossSection:
         pass
 
 class NeutrinoFlux:
-    def __init__(self, name: str, scaling: float,
+    def __init__(self, name, scaling: float,
                  neutrino_flavour: str, oscillation_mode: str):
         self.name = name
         self.scaling = scaling
@@ -91,11 +91,11 @@ class NeutrinoFlux:
 
         npoints = 1 if self.mode == FluxMode.Line else len(self.pdf_data) - 1
 
-        if self.oscillation_mode == OscillationMode.NoneMode:
+        if self.oscillation_mode == "none":
             self.flavours.clear()
             self.add_neutrino_flavour(NeutrinoFlavour.ElectronNeutrino, [1.0] * npoints)
 
-        elif self.oscillation_mode == OscillationMode.VacuumSunProduction:
+        elif self.oscillation_mode == "solar_vac_sun":
             p_ee = 1 - 0.5 * math.pow(math.sin(2 * theta_12), 2) * math.pow(math.cos(theta_13), 4) - 0.5 * math.pow(math.sin(2 * theta_13), 2)
             p_emu = -0.5 * math.sin(2 * theta_12) * math.pow(math.cos(theta_13), 2) * (math.sin(2 * theta_12) * (math.pow(math.sin(theta_23), 2) * math.pow(math.sin(theta_13), 2) - math.pow(math.cos(theta_23), 2)) - math.sin(2 * theta_23) * math.cos(2 * theta_12) * math.sin(theta_13)) + 0.5 * math.pow(math.sin(2 * theta_13), 2) * math.pow(math.sin(theta_23), 2)
             p_etau = 1 - p_ee - p_emu
@@ -105,7 +105,8 @@ class NeutrinoFlux:
             self.add_neutrino_flavour(NeutrinoFlavour.MuonNeutrino, [p_emu] * npoints)
             self.add_neutrino_flavour(NeutrinoFlavour.TauNeutrino, [p_etau] * npoints)
 
-        elif self.oscillation_mode == OscillationMode.MatterSunProduction:
+
+        elif self.oscillation_mode == "solar_matter_sun":
             # This part is specific and requires SolarMSWLookupTable
             # Initialise lookup table or load data from a file if it exists
             lookup_table_ee = DMCalcConstants.get_dmcalc_path() + "/DataBase/NeutrinoOscillations/lookup_table_solar_MSW_ee.txt"
@@ -199,6 +200,10 @@ class NeutrinoFlux:
         :return: Weighted average value.
         """
         # Map full neutrino names to their short forms
+
+        #print('made to flvavg')
+        #print(func(0.04), 'output')
+        
         full_to_short = {
             "ElectronNeutrino": "e",
             "MuonNeutrino": "mu",
@@ -238,33 +243,14 @@ class NeutrinoFlux:
         :return: Total flux (integrated over energy).
         """
         data = np.array(self.pdf_data)
-        #assuming first column (energy) in keV and second (flux) in MeV
-        flux = simps(x=data[:, 0], y=data[:, 1]*1e3) #NOTE this might be off by a factor
+
+        if len(data) == 1:
+            flux = data[:, 0] * data[:, 1]
+            flux = flux[0]
+        else:
+            #assuming first column (energy) in keV and second (flux) in MeV
+            flux = simps(x=data[:, 0]*1e-3, y=data[:, 1]) #NOTE this might be off by a factor
 
         # Result is in keV/cm^2
         return flux # Numerical integration using Simpson's rule
 
-
-# Example function to be used with flavour_average
-def example_function(x: float) -> float:
-    return x * 0.1  # Simple scaling function for demonstration
-
-# Main usage
-def main():
-    # Create a neutrino flux object
-    flux = NeutrinoFlux(name="8B", scaling=1.0, neutrino_flavour="e", oscillation_mode="solar_vac_sun")
-    #NOTE solar_matter_sun mode not fully implemented
-    
-    # Apply oscillations (this updates the flavours)
-    flux.apply_oscillation()
-
-    # Print the flavoured distribution after oscillations
-    print("Neutrino Flavours After Oscillation:", flux.flavours)
-
-    # Compute the flavour average for electron neutrino using a sample function
-    avg_flux = flux.flavour_average(example_function, "e")
-    print(f"Average flux for Electron Neutrino: {avg_flux}")
-
-
-if __name__ == "__main__":
-    main()

neutrinos/neutrino_rates.py
@@ -55,7 +55,7 @@ class CompositeNeutrinoFlux():
     def flavour_average(self, func, flavour):
         avg_for_flavour = 0
         for component in self.components:
-            component.oscillation_mode == OscillationMode.NoneMode
+            #component.oscillation_mode == OscillationMode.NoneMode
             component.apply_oscillation()
             avg = component.flavour_average(func, flavour)
             avg_for_flavour += avg
@@ -73,6 +73,28 @@ class NeutrinoRate:
         self.f_flux = CompositeNeutrinoFlux()
         self.set_interaction_type(interaction_type)
         self.set_required_fluxes(required_fluxes)
+
+    def spectrum(self):
+        flux_map: Dict[str, List[str]] = {
+            "DSN": ["dsnbflux_8", "dsnbflux_5", "dsnbflux_3"],
+            "Atmospheric": ["AtmNu_e", "AtmNu_ebar", "AtmNu_mu", "AtmNu_mubar"],
+            "8B": ["8B"],
+            "HEP": ["hep"],
+            "PEP": ["pep"],
+            "PP": ["pp"],
+            "CNO": ["13N", "15O", "17F"],
+            "7Be": ["7Be_384.3keV", "7Be_861.3keV"],
+            "7Be_PP_CNO": ["7Be_384.3keV", "7Be_861.3keV", "pp", "13N", "15O", "17F"]
+        }
+        for component in flux_map.keys():
+            required_neutrino_fluxes = flux_map.get(component, [])
+            for key in required_neutrino_fluxes:
+                flux = NeutrinoFlux(name=key, scaling=1.0, neutrino_flavour="e", oscillation_mode="solar_vac_sun",
+                                # "solar_vac_sun"
+                                    ) 
+                self.f_flux.add_component(flux)
+        return self.f_flux
+
 
     def set_required_fluxes(self, component: str):
         flux_map: Dict[str, List[str]] = {
@@ -93,6 +115,7 @@ class NeutrinoRate:
             flux = NeutrinoFlux(name=key, scaling=1.0, neutrino_flavour="e", oscillation_mode="solar_vac_sun",
                                # "solar_vac_sun"
                                 ) 
+            #flux.apply_oscillation()
             self.f_flux.add_component(flux)
 
     def set_interaction_type(self, interaction_type: InteractionType):
@@ -103,7 +126,7 @@ class NeutrinoRate:
             self.f_cross_section = NeutrinoCrossSectionElectroweakER(interaction_type) 
 
     def get_rate(self, recoil_keV: float) -> float:
-        m_e = 0.511  # Electron mass in keV
+        m_e = DMCalcConstants.m_e_keV  # Electron mass in keV
         rate = 0.0
 
         for flavour in ["ElectronNeutrino", "MuonNeutrino", "TauNeutrino"]:
@@ -121,150 +144,23 @@ class NeutrinoRate:
                     E_nu_min = 0.5 * (recoil_keV + math.sqrt(recoil_keV * (recoil_keV + 2 * m_e)))
 
                 # averages cross section over neutrino flux, weighted based on flavour
-                rate_function = lambda E_nu_keV: self.f_cross_section.dSigmadEr_cm2_keV(recoil_keV, E_nu_keV, nucleus, flavour) if E_nu_keV > E_nu_min else 0
-                        # keV/cm^2 [* conversion to keV *] weighted average cross section in cm2 keV
-                dR_dE_r = self.f_flux.get_total_flux_cm2s() * max(0, self.f_flux.flavour_average(rate_function, flavour))
-                dR_dE_r *= ((5.61e35 * 3.154e7)/ (m_nuc * 3.8292956500956284e-23)) #should give rate in per year per tonne
+
+                def rate_function(E_nu_keV):
+                    if E_nu_keV > E_nu_min:
+                        return self.f_cross_section.dSigmadEr_cm2_keV(recoil_keV, E_nu_keV, nucleus, flavour)
+                    else:
+                        return 0
+
+                #rate_function = lambda E_nu_keV: self.f_cross_section.dSigmadEr_cm2_keV(recoil_keV, E_nu_keV, nucleus, flavour) if E_nu_keV > E_nu_min else 0
+
+                # keV/cm^2 [* conversion to keV *] weighted average cross section in cm2 keV
+                #print(self.f_flux.get_total_flux_cm2s(), 'total flux')
+                #print(self.f_flux.flavour_average(rate_function, flavour), 'flvavg')
+                dR_dE_r = self.f_flux.get_total_flux_cm2s()* max(0, self.f_flux.flavour_average(rate_function, flavour))
+                dR_dE_r *= ((5.61e35 * 3.154e7)/ (m_nuc * 3.829295650095628e-26)) #should give rate in per year per tonne
                 #TODO check with Rob why is this in your code? what does 5.61e35 do? 
                 rate_contrib += nucleus.mass_frac * dR_dE_r
-
-                #print(flavour, rate_contrib)
+            
             rate += rate_contrib
-        #if self.f_interaction_type == InteractionType.COHERENT:
-         #   self.f_cross_section.form_factor_plot(self.target)
 
         return rate
-
-
-
-
-
-# Main usage
-def main2():
-    xe_target = Target("Xe", 131.29, 54)
-
-    # Define interaction type (e.g., COHERENT)
-    interaction_type = InteractionType.COHERENT
-
-    # Required flux component (e.g., "8B" neutrinos from the sun)
-    required_fluxes = "8B"
-
-    # Initialise the neutrino rate calculation for xe target
-    neutrino_rate = NeutrinoRate(required_fluxes, interaction_type, xe_target)
-
-    # Calculate the neutrino scattering rate for a specific recoil energy (in keV)
-    rates=[]
-    rates_per_keV=[]
-    recoil_energy_keV = np.logspace(-3, 2, 1000)  # Example recoil energy
-
-    for recoil_E in recoil_energy_keV:
-        r = neutrino_rate.get_rate(recoil_E)
-        rates.append(r)
-        rates_per_keV.append(r/recoil_E)
-    
-    diff_rate = np.array(rates) / (1e-38)
-
-    plt.clf()
-    plt.loglog(recoil_energy_keV, diff_rate)
-    
-    plt.xlabel('recoil energy [keV]')
-    plt.ylabel(f'rate [events/tonne/year/keV]e^{-38} ')
-    plt.xlim(0, 11)
-    plt.savefig("example_recoil_spec.png")
-    plt.show()
-
-
-    total_rate = simps(y=rates_per_keV, x=recoil_energy_keV)
-
-    print(f"Total Event Rate (events/tonne/year): {total_rate}")
-    # NOTE this DOESNT work - it gives you a very funky sin/cos graph
-
-
-
-
-def test_flux():
-    plt.figure(figsize = (12,8))
-    energies =np.logspace(-2, 4, 500)
-
-    xe_target = Target("Xe", 131.29, 54)
-
-    # Define interaction type (e.g., COHERENT)
-    interaction_type = InteractionType.COHERENT
-
-    # Required flux component (e.g., "8B" neutrinos from the sun)
-    required_fluxes = "DSN", "Atmospheric", "8B", "HEP", "PP", "PEP", "CNO", "7Be", "7Be_PP_CNO"
-
-    # Initialise the neutrino rate calculation for xe target
-    neutrino_rate = NeutrinoRate(required_fluxes, interaction_type, xe_target)
-
-
-    for name in required_fluxes:
-        neutrino_rate.set_required_fluxes(name)
-
-        flux = neutrino_rate.f_flux.components[0]
-
-        flux_pdf = np.array(flux.pdf_data)
-
-        energy, amp = flux_pdf[:,0]*1e-3, flux_pdf[:,1]
-
-        if (len(flux_pdf) == 1):
-            plt.plot([energy[0], energy[0]], [0, 1e5], label = name, ls = '--')
-        else:
-            plt.loglog((energies)[:len(amp)], (np.array(amp)*1e3)[:500], label = name)
-    #     plt.loglog(er, rate, label = name)
-    #plt.xlim(1e-3, 10000)
-    #plt.ylim(1, 1e12)
-    plt.xlabel('Neutrino Energy [MeV]', fontsize = 16)
-    plt.legend()
-    plt.ylabel('Flux [1/cm$^2$/s/MeV]', fontsize = 16)
-    plt.title('Neutrino Flux', fontsize = 20)
-    plt.savefig('NuFluxes_B8.pdf')
-    plt.show()
-
-
-def main():
-    xe_target = Target("Xe", 131.29, 54)
-
-    # Define interaction type (e.g., COHERENT)
-    interaction_type = InteractionType.COHERENT
-
-    # Required flux component (e.g., "8B" neutrinos from the sun)
-    required_fluxes = "DSN", "Atmospheric", "8B", "HEP", "PP", "PEP", "CNO", "7Be", "7Be_PP_CNO"
-
-    # Initialise the neutrino rate calculation for xe target
-    nurate = NeutrinoRate(required_fluxes, interaction_type, xe_target)
-    plt.figure(figsize = (12,8))
-
-    for name in required_fluxes:
-        nurate.set_required_fluxes(name)
-
-        total = 0
-        er = np.logspace(-2, 2, 1000)
-        rates = []
-        rates_per_keV = []
-        for e in er:
-            rate = nurate.get_rate(e)
-            rates.append(rate)
-            rates_per_keV.append(rate/e)
-            total += nurate.f_flux.get_total_flux_cm2s()
-        print('Total Flux: ', total, name)
-
-        #diff_rate = np.array(rates_per_keV) #TODO how do i get it per keV??
-
-        plt.loglog(er, rates, label = name)
-
-    
-    plt.xlabel('Recoil Energy [keV]', fontsize = 16)
-    plt.legend()
-    plt.ylabel('Rate [Events /ton/year/keV]', fontsize = 16)
-    plt.title('Neutrino Recoil Rates (NR)', fontsize = 20)
-    plt.grid(which = 'both')
-    plt.savefig("all.png")
-    plt.show()
-
-
-    test_flux()
-
-if __name__ == "__main__":
-    main()
-